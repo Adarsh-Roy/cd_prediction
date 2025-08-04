f03649467cb0655d54bbd5eda37a5550a09de9a7
Adarsh-Roy <79192126+Adarsh-Roy@users.noreply.github.com>
2025-08-04 20:23:31 +0530

    remove spare code of other models

--- training/src/models/model.py
+++ training/src/models/model.py
@@ -1,4 +1,3 @@
-from src.config.constants import DEFAULT_NUM_SLICES
 from src.models.experiment_models.model_PLM import Cd_PLM_Model
 
 
@@ -8,16 +7,11 @@ def get_model(
     slice_emb_dim: int = 256,
     design_emb_dim: int = 512,
     lstm_hidden_dim: int = 256,
-    k_neighbors: int = 16,
-    num_slices: int = DEFAULT_NUM_SLICES,
 ):
     """
     Return an instantiated Cd model based on model_type.
 
     - "plm" → PointNet + LSTM + MLP
-    - "dlm" → DGCNN + LSTM + MLP
-    - "ptm" → PointNet + Transformer + MLP [LATER]
-    - "dtm" → DGCNN + Transformer + MLP (uses dynamic slice batching) [LATER]
 
     Returns:
         nn.Module
@@ -29,14 +23,5 @@ def get_model(
             lstm_hidden_dim=lstm_hidden_dim,
             design_emb_dim=design_emb_dim,
         )
-    elif model_type == "dlm":
-        raise NotImplementedError("DLM is not implemented yet.")
-
-    elif model_type == "ptm":
-        raise NotImplementedError("PTM is not implemented yet.")
-
-    elif model_type == "dtm":
-        raise NotImplementedError("DTM is not implement yet.")
-
     else:
         raise ValueError(f"Unsupported model_type '{model_type}'. use 'plm'.")
